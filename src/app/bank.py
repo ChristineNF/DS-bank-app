47c23bce680396ece6090e13d5484b4bb1ee2730
Christine <christine_martens@hotmail.de>
2019-02-06 10:52:41 +0100

    Korrektur

--- src/app/bank.py
+++ src/app/bank.py
@@ -14,21 +14,17 @@ class Bank:
         self.accounts.append(account)
         return account
 
+    def _check_account_existance(self, account):
+        for acc in self.accounts:
+            if acc['firstname'] == account['firstname'] and acc['lastname'] == account['lastname']:
+                return True
+        return False
+
     def add_transaction(self, *, sender, recipient, subject, amount):
         assert amount > 0, 'Amount has to be greater than 0'
-        senderexists = False
-        recipientexists = False
-        for acc in self.accounts:
-            if acc['firstname'] == sender['firstname'] and acc['lastname'] == sender['lastname']:
-                senderexists = True
-                break
-        for acc in self.accounts:
-            if acc['firstname'] == recipient['firstname'] and acc['lastname'] == recipient['lastname']:
-                recipientexists = True
-                break
+        assert self._check_account_existance(sender), 'Sender has no account yet!'
+        assert self._check_account_existance(recipient), 'Recipient has no account yet!'
 
-        assert senderexists, 'Sender has no account yet!'
-        assert recipientexists, 'Recipient has no account yet!'
         transaction = {'sender': sender, 'recipient': recipient, 'subject': subject, 'amount': amount}
         self.transactions.append(transaction)
         return transaction
